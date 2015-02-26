047064e4a353cc2fb81fff697b8af1164d46cb8b
Russell Richardson <Russell@KronosAD.com>
2015-02-26 10:20:38 -0600

    Begin work on models

--- models/models.js
+++ models/models.js
@@ -6,11 +6,20 @@ module.exports = function() {
     function Class() {
         this.name = null;
         this.teacher = null;
+        this.owner = null;
+
+        this.assignments = [];
+
         this.color = null;
 
         this.save = function() {
             //console.log("YOU SAVED!");
         }
+    }
+
+    function Assignment() {
+        this.name = null;
+        this.details = null;
     }
 
     return { Class: Class };
