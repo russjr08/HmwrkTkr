12573d306db7f0da683c5edc8d0406b90fda400c
Russell Richardson <Russell@KronosAD.com>
2015-02-26 10:00:59 -0600

    Layout updates!
    Update the navigation bar to have a distinguishable color, and use the
    theme-color meta for Android Lollipop users.

--- routes/classes.js
+++ routes/classes.js
@@ -4,8 +4,6 @@ var router = express.Router();
 var Models = require('../models/models')();
 
 router.get('/manage/classes', function(req, res, next) {
-    var test = new Models.Class();
-    test.save();
     res.render('classes/manage_classes', { title: 'Classes', req: req });
 });
 
